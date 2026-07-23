1fb36b3bb84b3344cda6c350e5c0f0fec98fade8
Jackson Medeiros <jacksonmedeiros.dev@gmail.com>
2026-07-23 19:36:40 -0300

    chore: configura estrutura inicial do projeto

---
+++ src/main/java/br/com/jjnervosia/gerenciador_encomendas/GerenciadorEncomendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.jjnervosia.gerenciador_encomendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEncomendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorEncomendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jjnervosia/gerenciador_encomendas/GerenciadorEncomendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jjnervosia.gerenciador_encomendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEncomendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
